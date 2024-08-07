b47f5ecb5e25ca2e51c4d2c82d7100a11e81e380
Joe Prince <joe.admix@gmail.com>
2024-08-07 13:49:28 -0700

    Fix casing issue on authContext in frontend

--- frontend/src/layouts/components/MainNav.tsx
+++ frontend/src/layouts/components/MainNav.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from "../../contexts/AuthContext";
+import { useAuth } from "@/contexts/AuthContext";
 import MainNavAuth from "./MainNavAuth";
 import MainNavVisitor from "./MainNavVisitor";
 
